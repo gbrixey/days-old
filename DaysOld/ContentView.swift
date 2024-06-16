7b79536068ce5d95364147651457618423e97ed4
Glen Brixey <glen.brixey@gmail.com>
2024-06-16 12:23:14 -0500

    Initial Commit

---
+++ DaysOld/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DaysOld
+//
+//  Created by Glen Brixey on 6/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DaysOld/DaysOldApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysOldApp.swift
+//  DaysOld
+//
+//  Created by Glen Brixey on 6/16/24.
+//
+
+import SwiftUI
+
+@main
+struct DaysOldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
